ec5b23f441326e1ef348a4f91703d86a07773f0f
bgd <trifan.bogdan09@yahoo.com>
2024-11-23 13:18:28 +0200

    find libidn2

--- scripts/cmake.sh
+++ scripts/cmake.sh
@@ -44,7 +44,6 @@ configure() {
           -B "${BUILD_DIR}" \
           -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
           -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
-          -DCMAKE_PREFIX_PATH=/opt/homebrew \
           "${CMAKE_OPTS[@]}"
 }
 
